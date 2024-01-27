331b885d66ef57fe4fb8cab3103284f24f71e49e
imnotsome1ok <123349489+imnotsome1ok@users.noreply.github.com>
2024-01-26 18:51:47 -0800

    Update code.js

--- guessTheNumber/code.js
+++ guessTheNumber/code.js
@@ -1,1 +1,23 @@
 let num = Math.floor(Math.random() * 100);
+let guess;
+
+function guessCheck() {
+    guess = parseInt(document.getElementById("guessBox").value);
+
+    if (isNaN(guess)) {
+        document.getElementById("eval").textContent = "Please type a number in";
+    } else {
+        // Set the value of guessBox to the user's guess
+        document.getElementById("guessBox").value = guess;
+    }
+    
+    if(guess > num) {
+        document.getElementById("eval").textContent = "The number is lower";
+    } else if (guess < num) {
+        document.getElementById("eval").textContent = "The number is higher";
+    } else if (guess === num) {
+        document.getElementById("eval").textContent = "Good job you won! Reset the page to try again cause I ain't doing all that";
+    } else {
+        document.getElementById("eval").textContent = "Please type a number in";
+    }
+}
